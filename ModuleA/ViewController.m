cb0b86143678b76d5b1ccf067553f14c51a9ab02
Jack <781739973@qq.com>
2018-11-08 17:19:28 +0800

    初始化工程

---
+++ ModuleA/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  ModuleA
+//
+//  Created by Jack on 2018/11/8.
+//  Copyright © 2018年 bianla. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "PageBViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"我是模块B业务组件";
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, 0, 300, 100);
+    btn.backgroundColor = [UIColor greenColor];
+    btn.center = self.view.center;
+    [btn setTitle:@"模块B业务功能组件" forState: UIControlStateNormal];
+    [btn addTarget:self action:@selector(push) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+
+- (void)push {
+    PageBViewController *VC = [[PageBViewController alloc] init];
+    [self.navigationController pushViewController:VC animated:YES];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
